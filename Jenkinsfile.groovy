efd6295c57003dd4aa07450d4d5d1f6e67f403fa
ameni56 <ameni.hajri@esprit.tn>
2023-09-24 15:00:31 +0100

    Jenkinsfile

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,54 @@
+pipeline {
+    agent any
+    
+    environment {
+        // Define any environment variables needed for your build
+        JAVA_HOME = '/usr/lib/jvm/java-11-openjdk-amd64/'
+        MAVEN_HOME = 'opt/apache-maven-3.6.3'
+    }
+    
+    stages {
+        stage('Checkout') {
+            steps {
+                // Check out your Git repository
+                checkout scm
+            }
+        }
+        
+        stage('Build') {
+            steps {
+                // Build your Spring Boot application using Maven
+                sh "${MAVEN_HOME}/bin/mvn clean package"
+            }
+        }
+        
+        stage('Test') {
+            steps {
+                // Run tests if needed
+                sh "${MAVEN_HOME}/bin/mvn test"
+            }
+        }
+        
+        stage('Deploy') {
+            steps {
+                // You can add deployment steps here, such as deploying to a Tomcat server or a cloud platform
+                // For example, deploying to a Tomcat server:
+                // sh "${MAVEN_HOME}/bin/mvn tomcat7:redeploy"
+                
+                // Or deploying to a cloud platform like Heroku:
+                // sh "heroku deploy:jar --app your-heroku-app"
+            }
+        }
+    }
+    
+    post {
+        success {
+            // Notify on success, you can use various notification plugins or scripts here
+            echo 'Build and deployment successful!'
+        }
+        failure {
+            // Notify on failure and possibly take remedial actions
+            echo 'Build and deployment failed!'
+        }
+    }
+}
